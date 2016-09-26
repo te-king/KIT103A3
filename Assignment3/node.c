ef263a54fe770ca836b50a1a12ae2e45ac01a18d
te-king <thomas.e.king@icloud.com>
2016-09-26 12:32:14 +1000

    Initial Code

---
+++ Assignment3/node.c
@@ -0,0 +1,116 @@
+/*
+ * node ADT
+ * Author <<YOUR NAMES HERE>>
+ * Version September 2016
+ * 
+ * This file holds the Node ADT used by the Stack
+ *
+ * YOU NEED TO COMPLETE THIS FILE. 
+*/
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "assig_three216.h"
+#include "node.h"
+
+
+// the internals of the node
+struct node_int {
+	void *data;		// the value to be stored in the node
+	node next;		// a link to the next node in the list
+};
+
+
+/*
+	*	Node
+	*	initialiser function.
+	*	Pre-condition: none
+	*	Post-condition: the Node variable's "data" field ise
+	*					set to the given parameter (o) and the
+	*					Node variable's "next" field is set
+	*					to NULL
+	*	Informally: creates a linked-list node
+*/
+void init_node(node *n,void *o)
+{
+    trace("Node: Initialiser starts");
+
+COMPLETE ME
+
+    trace("Node: Initialiser ends");
+}
+
+/*
+	*	getDataN
+	*	Get function for variable's "data" value.
+	*	Pre-condition: n is not NULL
+	*	Post-condition: the value of the Node variable's "data"
+	*					field is returned
+	*	Informally: return the value within the node
+	*
+	*	return (void *) the data field of the node
+*/
+void *getDataN(node n)
+{
+    trace("TNode: getDataN starts and ends");
+
+COMPLETE ME
+}
+
+/*
+	*	getNextN
+	*	Get function for variable's "next" value.
+	*	Pre-condition: n is not NULL
+	*	Post-condition: the value of the Node variable's "next"
+	*					field is returned
+	*	Informally: return the pointer to the next node
+	*
+	*	return node the next field of the node
+*/
+node getNextN(node n)
+{
+    trace("TNode: getNextN starts and ends");
+
+	COMPLETE ME
+}
+
+/*
+	*	setDataN
+	*	Set function for the variable's "data" field.
+	*	Pre-condition: n is not NULL
+	*	Post-condition: the variable's "data" field is altered
+	*					to hold the given (o) value
+	*	Informally: assign the given value to the node variable
+	*
+	*	param (void *) o the value to place in the node
+*/
+void setDataN(node n,void *o)
+{
+    trace("TNode: setDataN starts");
+
+COMPLETE ME
+
+    trace("TNode: setDataN ends");
+}
+
+/*
+	*	setNextN
+	*	Set function for the variable's "next" field.
+	*	Pre-condition: v is not NULL
+	*	Post-condition: the variable's "next" field is altered
+	*					to hold the given (o) value
+	*	Informally: assign the given value to the node variable's
+	*				next field
+	*
+	*	param (node) n the value to place in the node's next field
+*/
+void setNextN(node v, node n)
+{
+    trace("TNode: setNextN starts");
+
+COMPLETE ME
+
+    trace("TNode: setNextN ends");
+}
